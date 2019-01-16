c7b7a4327a580ff22213f63acaee366071c9ea51
SPREADTEC\frederico.santos <frederico.santos@spread.com.br>
2019-01-16 16:29:04 -0200

    Created structure of the project. Closes #1

---
+++ finperson-web/src/main/java/br/com/finperson/FinPersonApplication.java
@@ -0,0 +1,14 @@
+package br.com.finperson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinPersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinPersonApplication.class, args);
+	}
+
+}
+
