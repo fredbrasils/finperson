02c985798b4576244d364e4c44c13cb1f9c076e6
SPREADTEC\frederico.santos <frederico.santos@spread.com.br>
2019-01-21 13:31:53 -0200

    created index page. #7

---
+++ finperson-web/src/main/java/br/com/finperson/controller/IndexController.java
@@ -0,0 +1,19 @@
+package br.com.finperson.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class IndexController {
+
+    
+    @RequestMapping({"/","/index","/index.html"})
+    public String home(Model model){
+    	log.debug("IndexController::home");
+        return "index";
+    }
+}
